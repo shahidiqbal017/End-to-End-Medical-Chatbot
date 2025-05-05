4f5e7cf9235fc4a05c9e3d6cec55fbc43527c569
Shahid Iqbal <sp22-bcs-017@cuivehari.edu.pk>
2025-05-05 09:41:38 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'shahidiqbal017',
+    author_email= 'sp22-bcs-017@cuivehari.edu.pk',
+    packages= find_packages(),
+    install_requires = []
+
+)
